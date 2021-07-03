055a5328219a0a2cac98d9852c8b010833ffddb1
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-07-03 11:38:27 -0700

    changed class component to function component, state to useStatate

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-export class Search extends Component {
-    state = {
-        text: ''
-    }
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired,
-    }
+const Search = ({searchUsers,clearUsers, showClear, setAlert}) => {
+    const [text, setText] = useState('')
+    
 
-    onSubmit =(e)=>{
+    const onSubmit =(e)=>{
         e.preventDefault();
-        if (this.state.text === ''){
-            this.props.setAlert('Please enter something', 'light');
+        if (text === ''){
+            setAlert('Please enter something', 'light');
         }else{
-            this.props.searchUsers(this.state.text)
-            this.setState({ text: ''})
+            searchUsers(text)
+            setText('')
 
         }
     }
 
-    onChange = (e) => this.setState({[e.target.name]: e.target.value})
+    const onChange = (e) => setText(e.target.value)
     
 
-    render() {
-        const {clearUsers, showClear} = this.props
+    
+        
         return (
             <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
+                <form onSubmit={onSubmit} className="form">
+                    <input type="text" name="text" placeholder="Search Users..." value={text} onChange={onChange} />
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
                 {showClear && 
@@ -41,7 +34,14 @@ export class Search extends Component {
                 }
             </div>
         )
-    }
+    
+}
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
 }
 
 
